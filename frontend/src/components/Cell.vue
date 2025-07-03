9413952b0ce299036b42485c2265a62f073e2352
Seth Aker <saker132@gmail.com>
2025-07-03 15:33:34 -0400

    Getting the front end off the ground

--- frontend/src/components/Cell.vue
+++ frontend/src/components/Cell.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
 import type { Cell } from '@/stores/models/cell';
+import useSudokuStore from '@/stores/sudoku'
+import { computed } from 'vue';
 const props = defineProps<{
   cell: Cell,
   width: number,
-  highlighted: boolean
+  highlighted: boolean,
+  selected: boolean,
 }>();
+const sudokuStore = useSudokuStore()
+const visiblePencilArray = computed(() => {
+  return new Array(sudokuStore.puzzle?.cellsPerRow).map((each, index) => {
+    return props.cell.pencilValues.includes(index);
+  })
+})
+
 </script>
 
 <template>
-  <div :class="[{ 'bg-orange-400': highlighted }, `w-[${props.width}px] h-[${props.width}px]`]">
-    <div v-if="cell.value !== undefined">{{ cell.value }}</div>
-
+  <div :class="[{ 'bg-orange-400': highlighted }, { 'bg-orange-600': selected }, `w-[${width}px] h-[${width}px]`]"
+    class="absolute">
+    <div v-if="cell.value !== undefined" class="relative h-full w-full"
+      :class="[{ 'font-bold': cell.type === 'original' }]">
+      {{ cell.value }}</div>
+    <div v-else class="relative h-full w-full flex flex-wrap">
+      <div v-for="(isVisable, index) in visiblePencilArray"
+        :class="[`w-1/${Math.sqrt(visiblePencilArray.length)} h-1/${Math.sqrt(visiblePencilArray.length)}`]">
+        <span v-show="isVisable" class="font-light">{{ index }}</span>
+      </div>
+    </div>
   </div>
 </template>
--- frontend/src/components/SudokuPuzzle.vue
+++ frontend/src/components/SudokuPuzzle.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import useSudokuStore from '@/stores/sudoku'
+import Cell from './Cell.vue';
 const sudokuStore = useSudokuStore()
-
+const cellWidth = 40;
+const generateCellOffset = (cellIndex: number) => {
+  let offset = cellWidth * cellIndex;
+  const blockLength = Math.sqrt(sudokuStore.puzzle?.cellsPerRow!);
+  offset += Math.floor(cellIndex / blockLength) * 3
+  return `left-[${offset}px]`
+}
 </script>
 
 <template>
   <div>
-    <div></div>
+    <div class="relative">
+      <div v-for="(row, index) in sudokuStore.puzzle?.rows" :key="`row${index}`">
+        <Cell v-for="(cell, index) in row" :key="`cell${index}`" :class="[generateCellOffset(index)]" :cell="cell"
+          :width="cellWidth" :highlighted="false" :selected="false" />
+      </div>
+
+    </div>
   </div>
 </template>
