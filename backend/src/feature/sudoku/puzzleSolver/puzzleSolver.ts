48594b83997a03afc20f3baccc0c80b1a347f40e
Seth Aker <saker132@gmail.com>
2026-04-17 00:07:33 -0400

    Cleaning up for production deployment

--- backend/src/feature/sudoku/puzzleSolver/puzzleSolver.ts
+++ backend/src/feature/sudoku/puzzleSolver/puzzleSolver.ts
@@ -1,9 +1,9 @@
-import { type Row } from "../datasource/models/row.ts";
-import { type StrategiesUsed } from "./strategies.ts";
+// import { type Row } from "../datasource/models/row.ts";
+// import { type StrategiesUsed } from "./strategies.ts";
 
-export class PuzzleSolver {
-  fillPuzzleCandidates: (puzzleRows?: Row[]) => Row[];
-  solvePuzzle: (puzzleRows?: Row[]) => {initialPuzzle: Row[], solvedPuzzle: Row[], strategiesUsed: StrategiesUsed}
-  isPuzzleSolved: (puzzleRows?: Row[], validate?: boolean) => boolean
-  numberWorksInCell: (rowIndex: number, colIndex: number, potentialNum: number, puzzleRows: Row[]) => boolean
-}
+// export class PuzzleSolver {
+//   fillPuzzleCandidates: (puzzleRows?: Row[]) => Row[];
+//   solvePuzzle: (puzzleRows?: Row[]) => {initialPuzzle: Row[], solvedPuzzle: Row[], strategiesUsed: StrategiesUsed}
+//   isPuzzleSolved: (puzzleRows?: Row[], validate?: boolean) => boolean
+//   numberWorksInCell: (rowIndex: number, colIndex: number, potentialNum: number, puzzleRows: Row[]) => boolean
+// }
