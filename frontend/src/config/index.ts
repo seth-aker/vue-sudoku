f020a9066c8583e0146344579c26606fa9fcbe05
Seth Aker <saker132@gmail.com>
2026-04-18 13:07:41 -0400

    GODDAMIT I MADE A TYPO

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
-  API_BASE_URL: 'https://sudoku.aker-berergon.dev/api',
+  API_BASE_URL: 'https://sudoku.aker-bergeron.dev/api',
   AUTH0_DOMAIN: "dev-sethaker.us.auth0.com",
   AUTH0_CLIENT_ID: "WxzvDOTTfq9QYUvlcIFkwe7DOhtglaP0"
 }
