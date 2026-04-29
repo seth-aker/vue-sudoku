ae363ecb07da1403a0970ea0de8fe316f89fff5e
Seth Aker <saker132@gmail.com>
2026-04-29 17:23:12 -0400

    Added backup script observability

--- backend/backup_script.sh
+++ backend/backup_script.sh
@@ -10,9 +10,17 @@ DATABASE_NAME=sudoku_db
 
 BACKUP_FILE="${BACKUP_DIR}/${DATABASE_NAME}_backup_${DATE}.sql"
 CLOUD_BACKUP_LOCATION="gs://sudoku-postgres-backup"
+echo " "
+echo "${date}: STARTING SUDOKU BACKUP"
 
-usr/bin/docker exec "$CONTAINER_NAME" pg_dump -U "$PG_USER" -d "$DATABASE_NAME" > "$BACKUP_FILE"
+echo "${date}: Dumping database in ${CONTAINER_NAME} to ${BACKUP_FILE}"
+/usr/bin/docker exec "$CONTAINER_NAME" pg_dump -U "$PG_USER" -d "$DATABASE_NAME" > "$BACKUP_FILE"
 
-gcloud storage cp "${BACKUP_FILE}" "${CLOUD_BACKUP_LOCATION}"
+/usr/bin/gcloud storage cp "${BACKUP_FILE}" "${CLOUD_BACKUP_LOCATION}"
+
+echo "${date} Removing local backups older than 7 days"
 
 find "$BACKUP_DIR" -type f -name "*.sql" -mtime +7 -exec rm {} \;
+
+echo " "
+echo "${date}: BACKUP COMPLETE"
