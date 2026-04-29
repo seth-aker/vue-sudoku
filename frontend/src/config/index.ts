371c07c9a53c30ed5f095a18b15710290b677f6d
Seth Aker <saker132@gmail.com>
2026-04-28 21:26:07 -0400

    Production setup

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,5 +1,4 @@
 export const config = {
   API_BASE_URL: import.meta.env.PROD ? 'https://sudoku.aker-bergeron.dev/api' : 'http://localhost:3666/api',
-  AUTH0_DOMAIN: "dev-sethaker.us.auth0.com",
-  AUTH0_CLIENT_ID: "WxzvDOTTfq9QYUvlcIFkwe7DOhtglaP0"
 }
+
