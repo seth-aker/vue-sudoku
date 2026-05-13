4cf9ca0ab6eb24230798d8284d0525c1d20b53f4
Seth Aker <saker132@gmail.com>
2026-05-12 23:44:51 -0400

    State management overhaul

--- frontend/src/composables/useSudokuGame.ts
+++ frontend/src/composables/useSudokuGame.ts
@@ -1,8 +1,31 @@
 import { useGameStore } from "@/stores/_gameStore";
-
+import { cloneDeep } from "lodash";
 export function useSudokuGame() {
   const gameStore = useGameStore()
   function selectCell(idx: number) {
     gameStore.selectedIdx = idx;
+  }
+  function togglePencil() {
+    gameStore.usingPencil = !gameStore.usingPencil
+  }
+  function placeValue(value: number, idx: number) {
+    if(value < 0 || value > 9 || idx < 0 || idx > 81) {
+      return;
+    }
+    if(gameStore.originalCells[idx].value !== 0) {
+      return;
+    }
+    const prevCell = cloneDeep(gameStore.cells[idx])
+    gameStore.actions.push({
+      prevCell,
+      idx: prevCell.idx,
+      isParent: true
+    })
+    gameStore.redoActions = []
+    gameStore.cells[idx].value = value;
+    if(prevCell.value === 0 && value === 0) {
+      
+    }
+
   }
 }
